07b448e184fd780c60b4a2b3b5fea96293bb8d39
Ganesh <ganesh@teleforce.in>
2025-02-13 12:16:16 +0530

    updated day-2

--- Day-2/resources.tf
+++ Day-2/resources.tf
@@ -1,6 +1,6 @@
 
 resource "azurerm_storage_account" "Testing-Storage-Account" {
-    name                     = "testingstorageaccount"
+    name                     = "testdemosa3421"
     resource_group_name      = azurerm_resource_group.Testing-Resource-Group.name
     location                 = azurerm_resource_group.Testing-Resource-Group.location
     account_tier             = "Standard"
