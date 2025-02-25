fe076148fbcf66cd84d512dc788e2436e6a63769
Ganesh <ganesh@teleforce.in>
2025-02-25 17:28:33 +0530

    Added Day-8 and 9

---
+++ Day-8/reources.tf
@@ -0,0 +1,6 @@
+
+resource "azurerm_resource_group" "telforce-rg" {
+  name     = "telforce-rg"
+  location = "East US"
+}
+
---
+++ Day-8/variables.tf
@@ -0,0 +1,35 @@
+variable "vnet_name" {
+  type        = string
+  default     = ""
+  description = "description"
+}
+
+variable "vnet_address_space" {
+    type        = list(string)
+    default     = ["10.0.0.0/16"]
+    description = "The address space that is used by the virtual network."
+}
+
+variable "prod_public_subnet_address" {
+  type        = list(string)
+  default     = []
+  description = "description"
+}
+
+variable "prod_private_subnet_address" {
+  type        = list(string)
+  default     = []
+  description = "description"
+}
+
+variable "prod_public_subnets" {
+  type        = list(string)
+  default     = []
+  description = "description"
+}
+
+variable "prod_private_subnets" {
+  type        = list(string)
+  default     = []
+  description = "description"
+}
