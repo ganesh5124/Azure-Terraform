78059c31c7ad90b5c90c8ee4a1cefb20f87d24f3
Ganesh5124 <pvgkumar2001@gmail.com>
2025-03-04 21:23:45 +0530

    modified day-3

---
+++ Day-3/README.m
@@ -0,0 +1,22 @@
+# Azure storage account creation
+
+* Create First Storage Account and container 
+* Because it will get error 
+* It checks the storage account and container in remote backend
+* Use Either backend.sh or  backend.tf but whenever you are using backend.tf comment the backend block create the resources of storage account and storage container 
+
+
+Backend in terraform?
+* Backend is used to store the state file in remote location
+
+State file in terraform?
+* State file is used to store the resources information which is created by terraform
+
+Terraform remote backend?
+* Terraform remote backend is used to store the state file in remote location
+
+What are the remote backend types in terraform?
+* 1. Azure Storage Account
+* 2. AWS S3 Bucket
+* 3. Google Cloud Storage Bucket
+* 4. HTTP
--- Day-3/README.md
+++
@@ -1,6 +0,0 @@
-# Azure storage account creation
-
-* Create First Storage Account and container 
-* Because it will get error 
-* It checks the storage account and container in remote backend
-* Use Either backend.sh or  backend.tf but whenever you are using backend.tf comment the backend block create the resources of storage account and storage container 
