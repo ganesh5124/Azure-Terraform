624e402e23ac2e095a7f51a0edd54c723b20588e
Ganesh5124 <pvgkumar2001@gmail.com>
2025-03-04 21:32:54 +0530

    modified day-7

--- Day-7/main.tf
+++ Day-7/main.tf
@@ -20,16 +20,7 @@ resource "azurerm_subnet" "subnet" {
     name                 = each.key
     resource_group_name  = azurerm_resource_group.rg[0].name
     virtual_network_name = azurerm_virtual_network.vnet.name
-<<<<<<< Updated upstream
-    address_prefixes     = [each.value]
-}
-
-# Example of provider meta-argument in azure resource
-provider "azurerm" {
-  features {}
-=======
     address_prefixes    = [each.value]
->>>>>>> Stashed changes
 }
 
 # Example for loop in terraform
