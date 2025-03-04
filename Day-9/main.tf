1ae22baf929c44edc48e3adf7fe4b2a0e635636d
Ganesh5124 <pvgkumar2001@gmail.com>
2025-03-04 22:44:05 +0530

    modified day-9 completed 3 assignments:

--- Day-9/main.tf
+++ Day-9/main.tf
@@ -1,4 +1,58 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "c5b0c396-1237-4eb0-84bd-3eee44556e69"
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "myResourceGroup"
   location = "East US"
 }
+
+# project naming convention
+# Your company requires all resource names to be lowercase and replace spaces with hyphen (-).
+locals {
+  project_name = "my project"
+  project_name_lower = replace(lower(local.project_name), " ", "-")
+}
+
+output "project_name_lower" {
+  value = local.project_name_lower
+}
+
+# Resource Tagging 
+# You need to combine default company tags with environment-specific tags.
+locals {
+  company_tags = {
+    owner = "admin"
+    environment = "dev"
+  }
+
+  environment_tags = {
+    environment = "prod"
+    cost_center = "12345"
+  }
+
+  all_tags = merge(local.company_tags, local.environment_tags)
+}
+
+output "all_tags" {
+  value = local.all_tags
+}
+# Azure storage account names must be less than 24 characters and use only lowercase letters and numbers by using substring function.
+locals {
+  storage_account_name = "my-storage-account"
+  updated_storage_name = substring(lower(local.storage_account_name), 0, 24)
+}
+resource "azurerm_storage_account" "storage" {
+  name                     = local.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  tags                     = local.all_tags
+}
+
+output "updated_storage_name" {
+  value = local.updated_storage_name
+}
+
